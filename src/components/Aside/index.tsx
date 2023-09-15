26965b1aad6571c335d70628382b5a8fcfaa3f29
Andre Flor <andreflor96@hotmail.com>
2023-09-14 23:27:11 -0300

    Aside

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useRef, useState } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { AsideContainer, MenuWrapper, Logo } from './styles';
+import LogoImg from '../../assets/logo.svg';
+// import LogoAside from '../../asssets/svg/logo-white-aside.svg';
+import { useAuth } from '../../providers/Auth';
+import {
+    FileText,
+    Gear,
+    House,
+    Kanban,
+    Package,
+    SignOut,
+    Tag,
+    Truck,
+} from 'phosphor-react';
+const Aside = () => {
+    const { pathname } = useLocation();
+    const navLinks = useRef<HTMLAnchorElement[]>([]);
+    const [topIndicator, setTopIndicator] = useState('0');
+    const [leftIndicator, setLeftIndicator] = useState('-4px');
+    const [hovered, setHovered] = useState(false);
+    const indicator = useRef<HTMLSpanElement>(null);
+    const { user, userLogoff } = useAuth();
+    const navigate = useNavigate();
+    const getDimensions = () => {
+        navLinks.current.forEach((item: HTMLAnchorElement) => {
+            if (item?.className === 'navlink--active') {
+                const top = item.offsetTop;
+                const left = item.offsetLeft;
+                setLeftIndicator(`${left}px`);
+                setTopIndicator(`${top}px`);
+            }
+        });
+    };
+
+    useEffect(() => {
+        setHovered(false);
+        getDimensions();
+
+        let timeoutId: NodeJS.Timeout;
+
+        const resizeListener = () => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => getDimensions(), 150);
+        };
+
+        window.onresize = () => {
+            resizeListener();
+        };
+
+        return () => {
+            window.onresize = () => {
+                resizeListener();
+            };
+        };
+    }, [pathname]);
+    return (
+        <AsideContainer
+            className={hovered ? 'hovered' : ''}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+            <Logo src={LogoImg} alt="Logo"></Logo>
+            <div>
+                <MenuWrapper
+                    topIndicator={topIndicator}
+                    leftIndicator={leftIndicator}
+                >
+                    <NavLink
+                        to="/dashboard"
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <House weight="fill" />
+                        <span>Dashboard</span>
+                    </NavLink>
+                    <NavLink
+                        to="/materiais"
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        {/* <img src={LogoAside} alt="materiais" /> */}
+                        <Tag weight="fill" />
+                        <span>Materiais</span>
+                    </NavLink>
+                    <NavLink
+                        to="/pedidos"
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <Package weight="fill" />
+                        <span>Pedidos</span>
+                    </NavLink>
+                    <NavLink
+                        to="/kanbans"
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <Kanban weight="fill" />
+                        <span>Kanbans</span>
+                    </NavLink>
+                    <NavLink
+                        to="/fornecedores"
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <Truck weight="fill" />
+                        <span>Fornecedores</span>
+                    </NavLink>
+                    <NavLink
+                        to={'/notas'}
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <FileText weight="fill" />
+                        <span>Notas</span>
+                    </NavLink>
+                    <NavLink
+                        to={'/configuracoes'}
+                        ref={(el: HTMLAnchorElement) =>
+                            navLinks.current.push(el)
+                        }
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <Gear weight="fill" />
+                        <span>Configurações</span>
+                    </NavLink>
+                    <NavLink
+                        to={'/'}
+                        onClick={userLogoff}
+                        className={({ isActive }) =>
+                            isActive ? 'navlink--active' : ''
+                        }
+                    >
+                        <SignOut weight="fill" />
+                        <span>Sair</span>
+                    </NavLink>
+                    <span className="indicator" ref={indicator}></span>
+                </MenuWrapper>
+            </div>
+        </AsideContainer>
+    );
+};
+
+export default Aside;
---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,172 @@
+import styled, { keyframes } from 'styled-components';
+
+interface IndicatorProps {
+    leftIndicator: string;
+    topIndicator: string;
+}
+const appearFromNowhere = keyframes`
+from{
+    opacity: 0;
+}
+
+to{
+    opacity: 1;
+}
+`;
+
+export const AsideContainer = styled.aside`
+    width: 100%;
+    height: 80px;
+
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 3;
+    display: flex;
+    justify-content: center;
+    background-color: var(--emerald-400);
+    border-radius: 0;
+
+    div {
+        width: 90%;
+        margin: 0 auto;
+    }
+
+    @media screen and (min-width: 720px) {
+        height: 100vh;
+        transition: width 0.4s;
+        border-radius: 0 24px 24px 0;
+        justify-content: flex-start;
+
+        width: 128px;
+
+        &.hovered {
+            width: 300px;
+            span {
+                padding-left: 12px;
+                font-size: 20px;
+                color: var(--gray-100);
+                font-weight: 300;
+                display: flex;
+                align-items: center;
+                animation: ${appearFromNowhere} 1s;
+            }
+        }
+
+        z-index: 3;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: var(--emerald-400);
+
+        padding: 150px 12px 0;
+    }
+`;
+
+export const MenuWrapper = styled.nav`
+    width: 100%;
+    height: 75%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    position: relative;
+    padding-right: 8px;
+
+    a {
+        display: flex;
+        align-items: flex-end;
+        justify-content: center;
+        position: relative;
+        z-index: 450;
+        padding: 0 6px 0;
+        margin-right: 8px;
+
+        span {
+            color: var(--gray-100);
+            font-weight: 600;
+            font-size: 18px;
+            display: none;
+        }
+
+        svg {
+            fill: var(--gray-100);
+            margin-right: 0;
+            width: 30px;
+            height: 30px;
+        }
+
+        &.navlink--active {
+            border-radius: 0;
+        }
+        &:active {
+            fill: var(--gray-100);
+        }
+    }
+
+    .indicator {
+        position: absolute;
+        left: ${(props: IndicatorProps) => props.leftIndicator};
+        top: 21px;
+        background-color: transparent;
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        border: 2px solid var(--gray-100);
+        z-index: 4;
+        transition: left 500ms;
+        transform: translateX(-4px);
+    }
+
+    @media screen and (min-width: 720px) {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        height: inherit;
+        padding-right: 0;
+        padding-bottom: 0px;
+
+        a {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            padding: 8px 24px;
+            position: relative;
+            z-index: 450;
+            margin-bottom: 18px;
+            margin-right: 0px;
+
+            svg {
+                fill: var(--gray-100);
+                margin-right: 8px;
+            }
+
+            &.navlink--active {
+                border-radius: 24px 0 0 24px;
+            }
+        }
+        .indicator {
+            position: absolute;
+            left: 14px;
+            top: ${(props) => props.topIndicator};
+            background-color: transparent;
+            height: 50px;
+            width: 50px;
+            border-radius: 50%;
+            border: 2px solid var(--gray-100);
+            z-index: 4;
+            transition: top 500ms;
+            transform: translateY(-2px);
+        }
+    }
+`;
+
+export const Logo = styled.img`
+    display: none;
+    @media screen and (min-width: 720px) {
+        display: block;
+        position: absolute;
+        top: 20px;
+        width: 50px;
+        align-self: center;
+    }
+`;
