ee7b87003ab0a865b0462c7d8b457b4b5a436b56
Andre Flor <andreflor96@hotmail.com>
2023-09-19 00:19:35 -0300

    Colocado demais rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,11 @@ import Login from '../components/Login';
 import Dashboard from '../pages/Dashboard';
 import ProtectedRoute from './route';
 import Settings from '../pages/Settings';
+import Products from '../pages/Products';
+import Orders from '../pages/Orders';
+import Kanbans from '../pages/Kanbans';
+import Suppliers from '../pages/Suppliers';
+import Invoices from '../pages/Invoices';
 const Rotas = () => {
     return (
         <Routes>
@@ -16,15 +21,57 @@ const Rotas = () => {
                         </ProtectedRoute>
                     }
                 />
-
                 <Route
-                    path="configuracoes"
+                    path="materiais"
                     element={
                         <ProtectedRoute>
-                            <Settings />
+                            <Products />
                         </ProtectedRoute>
                     }
                 />
+                <Route
+                    path="pedidos"
+                    element={
+                        <ProtectedRoute>
+                            <Orders />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="kanbans"
+                    element={
+                        <ProtectedRoute>
+                            <Kanbans />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="fornecedores"
+                    element={
+                        <ProtectedRoute>
+                            <Suppliers />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="notas"
+                    element={
+                        <ProtectedRoute>
+                            <Invoices />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route path="configuracoes">
+                    <Route
+                        path="usuario"
+                        element={
+                            <ProtectedRoute>
+                                <Settings />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Route>
             </Route>
         </Routes>
     );
