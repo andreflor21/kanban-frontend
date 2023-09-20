8f1e18046f217737e7fad9f6d8d20c02eb7360e8
Andre Flor <andreflor96@hotmail.com>
2023-09-20 15:46:47 -0300

    Redirecionamento do usuario nao logado para tela de login

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Aside from '../components/Aside';
 import HeaderNavAuth from '../components/HeaderNav';
-
+import { useAuth } from '../providers/Auth';
 const Root = () => {
-    return (
+    const { token } = useAuth();
+    return token ? (
         <>
             <Header>
                 <HeaderNavAuth />
@@ -12,6 +13,8 @@ const Root = () => {
             <Aside />
             <Outlet />
         </>
+    ) : (
+        <Navigate to="/login" /> // Mostrar pagina de não autorizado e colocar um botao para redirecionar a tela de Login
     );
 };
 
