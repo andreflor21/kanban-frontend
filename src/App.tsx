a8fa52dba153f89ccdac0b9d8420d46b368f979a
Andre Flor <andreflor96@hotmail.com>
2023-09-22 13:20:13 -0300

    Carregamento dos perfis

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import GlobalStyle from './style/global';
+import { useEffect } from 'react';
+import { usePerfil } from '../src/providers/Perfil';
 function App() {
+    const { getPerfis } = usePerfil();
+    useEffect(() => {
+        getPerfis();
+    }, []);
     return (
         <>
             <GlobalStyle themeDark={false} />
