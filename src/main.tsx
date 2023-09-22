f2c618235227a2ec1ad5f55f92f6a4c20f52b3d8
Andre Flor <andreflor96@hotmail.com>
2023-09-22 13:34:02 -0300

    Removido strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Providers>
-            <RouterProvider router={router} />
-            <App />
-        </Providers>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <Providers>
+        <RouterProvider router={router} />
+        <App />
+    </Providers>
+    // </React.StrictMode>
 );
