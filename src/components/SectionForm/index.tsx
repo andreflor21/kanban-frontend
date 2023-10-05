eff01120217584d26a2f6f356c2908ef0e6f7bc9
Andre Flor <andreflor96@hotmail.com>
2023-10-05 17:45:52 -0300

    Ajustes CSS Ajuste type Secao

--- src/components/SectionForm/index.tsx
+++ src/components/SectionForm/index.tsx
@@ -8,6 +8,7 @@ import {
     LabelStyled,
     SelectStyled,
     OptionStyled,
+    ContainerButtons,
 } from './styles';
 import Input from '../Input';
 import Button from '../Button';
@@ -26,7 +27,7 @@ interface SectionData {
     descricao: string;
     codigo: string;
     codigoMatrizFilial: string;
-    codigoERP: string | null;
+    codigoERP?: string;
     tipoSecaoId: number;
 }
 const SecitonForm = ({
@@ -39,6 +40,11 @@ const SecitonForm = ({
     const navigate = useNavigate();
     const [section, setSection] = useState<string>('');
     const [descricaoError, setDescricaoError] = useState<boolean>(false);
+    const [codigoError, setCodigoError] = useState<boolean>(false);
+    const [codigoMatrizFilialError, setCodigoMatrizFilialError] =
+        useState<boolean>(false);
+    const [codigoERPError, setcodigoERPError] = useState<boolean>(false);
+    const [tipoError, setTipoError] = useState<boolean>(false);
     const [, setLoad] = useState(true);
     const { newSection, editSection, sections } = useSection();
 
@@ -52,7 +58,7 @@ const SecitonForm = ({
     const [codigoMatrizFilial, setCodigoMatrizFilial] = useState(
         s?.codigoMatrizFilial
     );
-    const [tipoSecaoId, setTipoSecaoId] = useState(s?.tipoSecao.id);
+    const [tipoSecaoId, setTipoSecaoId] = useState(s?.tipoSecao.id.toString());
 
     const goBack = (path: string) => {
         navigate(path);
@@ -63,17 +69,17 @@ const SecitonForm = ({
         const updateObject: SectionData = {
             descricao: descricao as string,
             codigo: codigo as string,
-            codigoERP: codigoERP as string | null,
+            codigoERP: codigoERP as string,
             codigoMatrizFilial: codigoMatrizFilial as string,
-            tipoSecaoId: tipoSecaoId as number,
+            tipoSecaoId: tipoSecaoId as string,
         };
 
         const schema = yup.object().shape({
             descricao: yup.string().required('Campo Obrigatório'),
             codigo: yup.string().required('Campo Obrigatório'),
-            codigoERP: yup.string().required('Campo Obrigatório'),
+            codigoERP: yup.string(),
             codigoMatrizFilial: yup.string().required('Campo Obrigatório'),
-            tipoSecaoId: yup.number().required('Campo Obrigatório'),
+            tipoSecaoId: yup.string().required('Campo Obrigatório'),
         });
 
         await schema
@@ -82,11 +88,14 @@ const SecitonForm = ({
                 if (action === 'create') {
                     newSection(v, setLoad, navigate);
                 } else if (action === 'edit' && sectionId) {
-                    editSection(v, parseInt(sectionId));
+                    editSection(v, sectionId);
                 }
             })
             .catch(() => {
                 descricao === '' && setDescricaoError(true);
+                codigo === '' && setCodigoError(true);
+                codigoMatrizFilial === '' && setCodigoMatrizFilialError(true);
+                !tipoSecaoId && setTipoError(true);
             });
     };
     return (
@@ -108,6 +117,45 @@ const SecitonForm = ({
                             setDescricaoError(false);
                         }}
                     />
+                    <Input
+                        label="Código"
+                        inputType="text"
+                        placeholder="Código"
+                        errorMessage="Campo Obrigatório"
+                        value={codigo}
+                        error={codigoError}
+                        name="codigo"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setCodigo(e.target.value);
+                            setCodigoError(false);
+                        }}
+                    />
+                    <Input
+                        label="Código ERP"
+                        inputType="text"
+                        placeholder="Código ERP"
+                        // errorMessage="Campo Obrigatório"
+                        value={codigoERP}
+                        // error={codigoERPError}
+                        name="codigoERP"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setCodigoERP(e.target.value);
+                            // setCodigoERPError(false);
+                        }}
+                    />
+                    <Input
+                        label="Código Matriz/Filial"
+                        inputType="text"
+                        placeholder="Código Matriz/Filial"
+                        errorMessage="Campo Obrigatório"
+                        value={codigoMatrizFilial}
+                        error={codigoERPError}
+                        name="codigoMatrizFilial"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setCodigoMatrizFilial(e.target.value);
+                            setCodigoMatrizFilialError(false);
+                        }}
+                    />
 
                     <ContainerSelect>
                         <LabelStyled htmlFor="section">
@@ -116,11 +164,12 @@ const SecitonForm = ({
                         <SelectStyled
                             id="section"
                             name="section"
-                            value={section}
+                            value={tipoSecaoId}
                             onChange={(
                                 e: React.ChangeEvent<HTMLSelectElement>
                             ) => {
-                                setSection(e.target.value);
+                                setTipoSecaoId(e.target.value);
+                                setTipoError(false);
                             }}
                         >
                             <OptionStyled value="">
@@ -133,22 +182,25 @@ const SecitonForm = ({
                             ))}
                         </SelectStyled>
                     </ContainerSelect>
-                    <Button
-                        type="button"
-                        onClickFunc={(e: React.MouseEvent<HTMLButtonElement>) =>
-                            handleSubmit(e)
-                        }
-                    >
-                        Gravar
-                    </Button>
+                    <ContainerButtons>
+                        <Button
+                            type="button"
+                            className="voltar button1"
+                            onClickFunc={() => goBack('/secoes')}
+                        >
+                            Voltar
+                        </Button>
+                        <Button
+                            type="button"
+                            className="button2"
+                            onClickFunc={(
+                                e: React.MouseEvent<HTMLButtonElement>
+                            ) => handleSubmit(e)}
+                        >
+                            Gravar
+                        </Button>
+                    </ContainerButtons>
                 </FormStyled>
-                <Button
-                    type="button"
-                    className="voltar"
-                    onClickFunc={() => goBack('/secoes')}
-                >
-                    Voltar
-                </Button>
             </Container>
         </>
     );
