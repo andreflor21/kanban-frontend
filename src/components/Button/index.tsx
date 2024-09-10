b95e919008d82eb8e44be6c71918a457cfd54a57
Richard Marllon <richard_marllon@hotmail.com>
2024-09-10 11:31:08 -0300

    style: padroniza espaçamento em imports em vários arquivos
    Padronização do espaçamento automático entre chaves em importações across o projeto, melhorando a consistência de estilo de código e sua legibilidade.
    
    - Ajusta espaçamento ao redor de chaves em arquivos TypeScript
    - Melhora a consistência do código em componentes de interface
    - Inclui arquivos de configuração Vite e de rotas
    - Corrige espaçamento em imports de pacotes externos
    - Modifica arquivos de componentes, páginas, serviços e roteamento

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,10 @@
-import type React, {ReactNode} from "react"
-import {ButtonItem} from "./styles"
+import type React from "react"
+import { ButtonItem } from "./styles"
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	onClickFunc?: (e: React.MouseEvent<HTMLButtonElement>) => void
 	// whiteSchema?: boolean;
-	children: ReactNode
+	children: React.ReactNode
 	type?: "button" | "submit" | "reset" | undefined
 }
 
